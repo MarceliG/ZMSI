4d3b06790e87b0373f7cf2c4def7a0567c6397f1
MarceliG <marcel9817@gmail.com>
2024-12-03 21:20:42 +0100

    optimize

--- main.py
+++ main.py
@@ -27,39 +27,36 @@ def main() -> None:
         dataset_preprocessed = preprocessor.preprocess()
         save_dataset(dataset_preprocessed, FilePath.dataset_preprocessed)
 
-    if args.train:
+    if args.train or args.evaluate:
         dataset = load_dataset_from_path(FilePath.dataset_preprocessed)
-        tokenizer = Tokenizer()
-
         max_length = 512
-        tokenized_train_dataset = tokenizer.tokenize(dataset["train"], max_length=max_length)  # 512 za długo się uczy
-        tokenized_test_dataset = tokenizer.tokenize(dataset["test"], max_length=max_length)  # 512 za długo się uczy
-        tokenized_validation_dataset = tokenizer.tokenize(
-            dataset["validation"], max_length=max_length
-        )  # 512 za długo się uczy
+        batch_size = 8
+        tokenizer = Tokenizer()
         trainer = TrainerModel()
+
+        tokenized_train_dataset = tokenizer.tokenize(dataset["train"], max_length=max_length)
+        tokenized_test_dataset = tokenizer.tokenize(dataset["test"], max_length=max_length)
+        tokenized_validation_dataset = tokenizer.tokenize(dataset["validation"], max_length=max_length)
+
+    if args.train:
         trainer.train(
             tokenized_train_dataset=tokenized_train_dataset,
             tokenized_validation_dataset=tokenized_validation_dataset,
             model_path_to_save=FilePath.models,
             num_labels=5,
             epoch=10,
-            batch_size=8,
+            batch_size=batch_size,
             learning_rate=5e-5,
         )
 
     if args.evaluate:
-        dataset = load_dataset_from_path(FilePath.dataset_preprocessed)
-        tokenizer = Tokenizer()
         model = load_model_from_disc(FilePath.models)
-        max_length = 512
-        tokenized_test_dataset = tokenizer.tokenize(dataset["test"], max_length=max_length)
-        trainer = TrainerModel()
+
         trainer.evaluate_trained_model(
             model,
             tokenized_dataset=tokenized_test_dataset,
             result_path_to_save=FilePath.classification_report_path,
-            batch_size=8,
+            batch_size=batch_size,
         )
 
     logger.info("Finish application")
